b9da9d96a54bcc3f2178c77c292c835411227022
Ehab Borae Hassanien <68748080+ehaborae@users.noreply.github.com>
2022-03-28 13:03:02 +0200

    a

--- screens/MealsOverViewScreen.js
+++ screens/MealsOverViewScreen.js
@@ -1,15 +1,32 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import Meal from '../component/MealComponent';
 import { CATEGORIES } from '../data/dummy-data';
+import { MEALS } from '../data/dummy-data';
 
 function MealsOverViewScreens({ route }) {
     const catId = route.params.categoryId;
     const catTitle = route.params.categoryTitle;
 
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catId) >= 0;
+    });
+
+    function renderMealItem(itemData) {
+        return (
+            <Meal title={itemData.item.title} />
+        );
+    }
+
     return (
         <View style={
             style.container
         }>
-            <Text> {catTitle} Meals OverView Screens {catId} </Text>
+            <FlatList
+                data={displayedMeals}
+                keyExtractor={(item) => item.id}
+                renderItem={renderMealItem}
+
+            />
         </View>
     );
 }
